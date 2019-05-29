72f1494f3470b5e5cbe0700c2c802bcd65a4c60b
stereobooster <stereobooster@gmail.com>
2019-05-29 22:48:08 +0200

    Add markup according to WAI-ARIA spec

--- src/App.js
+++ src/App.js
@@ -6,9 +6,38 @@ function App() {
   return (
     <>
       <Accordion>
-        <AccordionSection title="section 1">content 1</AccordionSection>
-        <AccordionSection title="section 1" expanded>
-          content 2
+        <AccordionSection title="section 1" id="1">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius
+            lobortis iaculis. Donec ornare tellus vel quam convallis, quis
+            tristique libero blandit. Morbi quis lacinia urna. Curabitur laoreet
+            in tortor ac faucibus. Duis laoreet eros nulla, eget cursus elit
+            mollis id. Fusce sagittis dui vitae malesuada imperdiet. In non
+            facilisis dui. Duis sit amet nulla eu odio venenatis posuere. Nam
+            congue luctus risus nec laoreet. Quisque rutrum porttitor sapien,
+            nec iaculis eros pharetra volutpat. Maecenas fringilla ultrices
+            massa vitae interdum.
+          </p>
+        </AccordionSection>
+        <AccordionSection title="section 2" id="2" expanded>
+          <p>
+            <a href="/">test link</a>
+            Suspendisse lobortis diam quis magna faucibus, in volutpat eros
+            facilisis. Aliquam ac mattis tellus, vitae tincidunt purus. Sed
+            lectus justo, maximus eget egestas eu, congue et enim. Maecenas
+            ullamcorper sollicitudin turpis, condimentum interdum nisl. Proin
+            leo dolor, varius eu sagittis eu, scelerisque nec dolor. Sed velit
+            diam, consectetur vel placerat vel, egestas nec tellus. Ut convallis
+            accumsan ipsum id consectetur. Morbi lorem purus, condimentum et
+            tellus non, volutpat ultricies risus. Nam pellentesque risus eu odio
+            bibendum, ut vehicula arcu convallis. Quisque interdum ligula ac
+            vulputate ornare. Praesent condimentum ligula sapien, id placerat
+            tellus sollicitudin sit amet. Pellentesque et urna sed sem porttitor
+            maximus. Pellentesque ullamcorper ipsum id turpis eleifend,
+            dignissim tincidunt dolor lobortis. Maecenas id elit et est vehicula
+            imperdiet. In non diam ultrices ex sagittis dapibus nec rutrum eros.
+            Nunc id egestas lorem.
+          </p>
         </AccordionSection>
       </Accordion>
     </>
--- src/components/AccordionSection.js
+++ src/components/AccordionSection.js
@@ -1,8 +1,28 @@
 import React from "react";
 
-export const AccordionSection = ({ children, title, expanded }) => (
-  <>
-    <div>{title}</div>
-    <div>{expanded && children}</div>
-  </>
-);
+export const AccordionSection = ({ children, title, expanded, id }) => {
+  const sectionId = `section-${id}`;
+  const labelId = `label-${id}`;
+
+  return (
+    <>
+      <div
+        role="button"
+        aria-expanded={expanded}
+        aria-controls={sectionId}
+        id={labelId}
+        tabIndex={0}
+      >
+        {title}
+      </div>
+      <div
+        role="region"
+        aria-labelledby={labelId}
+        id={sectionId}
+        hidden={!expanded}
+      >
+        {expanded && children}
+      </div>
+    </>
+  );
+};
