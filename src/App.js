ec10548841990bafa59f757b3881fb8c461ccc5c
stereobooster <stereobooster@gmail.com>
2019-06-02 14:58:59 +0200

    Render prop for lazy rendering

--- src/App.js
+++ src/App.js
@@ -63,25 +63,28 @@ function App() {
         </AccordionSection>
         {test && (
           <AccordionSection title="section 3">
-            <p>
-              <a href="/">test link</a>
-              Suspendisse lobortis diam quis magna faucibus, in volutpat eros
-              facilisis. Aliquam ac mattis tellus, vitae tincidunt purus. Sed
-              lectus justo, maximus eget egestas eu, congue et enim. Maecenas
-              ullamcorper sollicitudin turpis, condimentum interdum nisl. Proin
-              leo dolor, varius eu sagittis eu, scelerisque nec dolor. Sed velit
-              diam, consectetur vel placerat vel, egestas nec tellus. Ut
-              convallis accumsan ipsum id consectetur. Morbi lorem purus,
-              condimentum et tellus non, volutpat ultricies risus. Nam
-              pellentesque risus eu odio bibendum, ut vehicula arcu convallis.
-              Quisque interdum ligula ac vulputate ornare. Praesent condimentum
-              ligula sapien, id placerat tellus sollicitudin sit amet.
-              Pellentesque et urna sed sem porttitor maximus. Pellentesque
-              ullamcorper ipsum id turpis eleifend, dignissim tincidunt dolor
-              lobortis. Maecenas id elit et est vehicula imperdiet. In non diam
-              ultrices ex sagittis dapibus nec rutrum eros. Nunc id egestas
-              lorem.
-            </p>
+            {() => (
+              // I don't know how to disable warning (Functions are not valid as a React child)
+              <p>
+                <a href="/">test link</a>
+                Suspendisse lobortis diam quis magna faucibus, in volutpat eros
+                facilisis. Aliquam ac mattis tellus, vitae tincidunt purus. Sed
+                lectus justo, maximus eget egestas eu, congue et enim. Maecenas
+                ullamcorper sollicitudin turpis, condimentum interdum nisl.
+                Proin leo dolor, varius eu sagittis eu, scelerisque nec dolor.
+                Sed velit diam, consectetur vel placerat vel, egestas nec
+                tellus. Ut convallis accumsan ipsum id consectetur. Morbi lorem
+                purus, condimentum et tellus non, volutpat ultricies risus. Nam
+                pellentesque risus eu odio bibendum, ut vehicula arcu convallis.
+                Quisque interdum ligula ac vulputate ornare. Praesent
+                condimentum ligula sapien, id placerat tellus sollicitudin sit
+                amet. Pellentesque et urna sed sem porttitor maximus.
+                Pellentesque ullamcorper ipsum id turpis eleifend, dignissim
+                tincidunt dolor lobortis. Maecenas id elit et est vehicula
+                imperdiet. In non diam ultrices ex sagittis dapibus nec rutrum
+                eros. Nunc id egestas lorem.
+              </p>
+            )}
           </AccordionSection>
         )}
       </Accordion>
